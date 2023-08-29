a6be25bf5acf6e0a6bba5ae976c8ad6711d3ac5a
Issam70 <issamsaghe60@gmail.com>
2023-08-29 21:16:51 +0200

    hh

--- App.js
+++ App.js
@@ -3,15 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen'; // Create this component
 import ProductScreen from './screens/ProductScreen';
-
+import CustomersOrders from './screens/CustomersOrders';
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="CustomersOrders">
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" Component={RegisterScreen} />
+        <Stack.Screen name="CustomersOrders" component={CustomersOrders}/>
         <Stack.Screen name="Product" component={ProductScreen} />
         {/* Add more screens here */}
       </Stack.Navigator>
